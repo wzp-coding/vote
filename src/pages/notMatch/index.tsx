a59a4365211670c860974593fb73eae5d71e63f8
wuzepeng <wuzepeng@cvte.com>
2023-07-25 16:37:01 +0800

    feat: 补充 Not Found 页面

--- src/pages/notMatch/index.tsx
+++ src/pages/notMatch/index.tsx
@@ -1,5 +1,14 @@
+import { ErrorBlock } from "antd-mobile";
 import "./index.less";
 
 export default function NoMatch() {
-  return <div className='no-match'>404</div>;
+  return (
+    <div className='no-match'>
+      <ErrorBlock
+        status='empty'
+        description='404 Not Found'
+        title='找不到页面，请检查网址是否正确'
+      />
+    </div>
+  );
 }
